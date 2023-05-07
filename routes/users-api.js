07df9364e458cb469b861e157661949600d0962f
lianaa98 <liana.kan98@gmail.com>
2023-05-07 14:49:48 -0700

    fetched data from api

--- routes/users-api.js
+++ routes/users-api.js
@@ -9,16 +9,34 @@ const express = require('express');
 const router  = express.Router();
 const userQueries = require('../db/queries/users');
 
-router.get('/', (req, res) => {
-  userQueries.getUsers()
-    .then(users => {
-      res.json({ users });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  organization: process.env.OPENAI_ORGANIZATION,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
+router.post('/', async (req, res) => {
+  // userQueries.getUsers()
+  //   .then(users => {
+  //     res.json({ users });
+  //   })
+  //   .catch(err => {
+  //     res
+  //       .status(500)
+  //       .json({ error: err.message });
+  //   });
+  console.log("hello")
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Say this is a test",
+    max_tokens: 7,
+    temperature: 0,
+  });
+  console.log("response:", response.data.choices[0].text)
+
+
 });
 
 module.exports = router;
