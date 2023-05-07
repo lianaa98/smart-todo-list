2c8c6c6b83e1ded5cded708a83d228e115a79f90
lianaa98 <liana.kan98@gmail.com>
2023-05-07 14:57:59 -0700

    clean up

--- routes/test/openai.js
+++
@@ -1,8 +0,0 @@
-import { Configuration, OpenAIApi } from "openai";
-const configuration = new Configuration({
-    organization: "org-SycRMZ9IeWf7HpVSY4jn2Co4",
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-// const response = await openai.listEngines();
-
