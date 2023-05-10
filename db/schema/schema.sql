a68693604ed9d85d32690c7ebd646f6ad9e0f8cd
abeljlim <subrosian@hotmail.com>
2023-05-09 20:28:01 -0700

    /users/profile GET and POST routesand table update with motto and avatar

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -7,6 +7,8 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
+  avatar_id INTEGER NOT NULL DEFAULT 1,
+  motto TEXT,
   password VARCHAR(255) NOT NULL
 );
 
